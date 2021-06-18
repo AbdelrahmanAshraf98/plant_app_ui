9dec81ff0310b90133d176de5cf7e0a0205c5580
Abd ElRahman Ashraf <33667787+AbdelrahmanAshraf98@users.noreply.github.com>
2021-06-18 02:53:49 +0200

    test commit
    It's a test commit

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -8,6 +8,7 @@ import 'header_with_searchbox.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int text = 0;
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
